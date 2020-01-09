b190a063fd6bb12d9aa135f54ea6e5b03e9e11b2
ErezMizrahi <erezm@webtech-inv.co.il>
2020-01-09 08:35:01 +0200

    Initial Commit

---
+++ Filters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Filters
+//
+//  Created by Erez Mizrahi on 09/01/2020.
+//  Copyright © 2020 com.erez8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
